4bc2c8cc131fa720cb08ed3cdd4a431c190c4510
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-11-14 23:14:27 +0100

    Changed argument error msg in ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 21:32:50 by juestrel          #+#    #+#             */
-/*   Updated: 2024/11/14 23:03:25 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/11/14 23:14:12 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main (int argc, char *argv[])
 {
 	if (argc != 2 || argv[1] == NULL)
-		return std::cerr << "You must provide at least one argument" << std::endl, 1;
+		return std::cerr << "You must provide only one argument" << std::endl, 1;
 	RPN rpn;
 	std::cout << rpn.rpnCalculation(argv[1]) << std::endl;
 	return (0);
