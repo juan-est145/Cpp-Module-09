70aa52943b86b72dd244abfc3b059ec18f51c184
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-11-15 21:50:23 +0100

    Changed implementation back to template

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -8,18 +8,50 @@
 #include <cstdlib>
 #include <climits>
 
+template <typename T>
 class PmergeMe
 {
 private:
-	std::vector<int> _vector;
-	std::deque<int> _deque;
+	T _container;
 
 public:
 	PmergeMe(void);
-	PmergeMe(int argc, char *argv[]);
 	PmergeMe(const PmergeMe &copy);
 	PmergeMe &operator=(const PmergeMe &assign);
+	void push_back(int number);
+	void mergeInsertSort(void);
 	~PmergeMe(void);
 };
+
+template <typename T>
+PmergeMe<T>::PmergeMe(void) {}
+
+template <typename T>
+PmergeMe<T>::PmergeMe(const PmergeMe<T> &copy) 
+{
+	*this = copy;
+}
+
+template <typename T>
+PmergeMe<T> &PmergeMe<T>::operator=(const PmergeMe<T> &assign) 
+{
+	if (this != &assign)
+		this->_container = assign._container;
+	return *this;
+}
+
+template <typename T>
+void PmergeMe<T>::push_back(int number)
+{
+	this->_container.push_back(number);
+}
+template <typename T>
+void PmergeMe<T>::mergeInsertSort(void)
+{
+
+}
+
+template <typename T>
+PmergeMe<T>::~PmergeMe(void) {}
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,19 +6,52 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 20:31:27 by juestrel          #+#    #+#             */
-/*   Updated: 2024/11/15 20:52:56 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/11/15 21:45:49 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-int main(int argc, char *argv[])
+static void valArgs(int argc, char *argv[], PmergeMe<std::vector<int> > &vector, PmergeMe<std::deque<int> > &deque)
+{
+	for (size_t i = 1; i < (unsigned int)argc; i++)
+	{
+		for (size_t j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] == '+')
+                j++;
+            if (argv[i][0] == '0')
+            {
+                std::cout << "Error: 0 cannot be present as a number" << std::endl;
+                exit(1);
+            }
+            else if (!isdigit(argv[i][j]))
+            {
+                std::cout << "Error, invalid character in sequence" << std::endl;
+                exit(1);
+            }
+		}
+		long number = atol(argv[i]);
+		if (number > INT_MAX || number <= 0)
+		{
+			std::cout << "Error: number is either too big for int or a negative" << std::endl;
+            exit(1);
+		}
+		vector.push_back(static_cast<int>(number));
+		deque.push_back(static_cast<int>(number));
+	}
+}
+
+	int main(int argc, char *argv[])
 {
 	if (argc == 1)
 	{
 		std::cout << "Error, you need to provide at least a number as an argument" << std::endl;
 		return (1);
 	}
-	PmergeMe merge(argc, argv);
+	PmergeMe<std::vector<int> > vector;
+	PmergeMe<std::deque<int> > deque;
+
+	valArgs(argc, argv, vector, deque);
 	return (0);
 }
